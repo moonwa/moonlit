190693ff2b107ae65a085ab0e6ebf2692621e080
moon.wa <moon.ww.z@gmail.com>
2015-02-01 08:45:03 +0800

    fix nuget versions

--- src/moonlit/Properties/AssemblyInfo.cs
+++ src/moonlit/Properties/AssemblyInfo.cs
@@ -18,9 +18,7 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("9a1dfdcf-3a76-4055-b495-f460e88e7f03")]
-[assembly: InternalsVisibleTo("Moonlit.TestFixtures, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f59fc8d901513a6615ed32c7015dab76bccb1be3f9d8de652a69761854ed57fa293025e10ba17bed8217bfbc028a8756e0ca3531d815c26d2b0e091579c4d4cc2990aea6deb673091784500e732c59271b7e48f7498686ba78709844a420603b03e1a40e774a8658d34e13e62d87156008428aa89137b249910fea74d54d7e9c")]
-//[assembly: InternalsVisibleTo("Moonlit.TestFixtures, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f59fc8d901513a6615ed32c7015dab76bccb1be3f9d8de652a69761854ed57fa293025e10ba17bed8217bfbc028a8756e0ca3531d815c26d2b0e091579c4d4cc2990aea6deb673091784500e732c59271b7e48f7498686ba78709844a420603b03e1a40e774a8658d34e13e62d87156008428aa89137b249910fea74d54d7e9c")]
-
+ 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -30,5 +28,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.1.1.2")]
+[assembly: AssemblyVersion("1.1.4")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
