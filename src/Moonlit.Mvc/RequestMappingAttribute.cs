03a15801b4fde60e48e2d2d1da2c9d869aad77b6
moon.wa <moon.ww.z@gmail.com>
2015-03-02 18:08:27 +0800

    add a

---
+++ src/Moonlit.Mvc/RequestMappingAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moonlit.Mvc
+{
+    public class RequestMappingAttribute : Attribute
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+
+        public RequestMappingAttribute()
+        {
+
+        }
+        public RequestMappingAttribute(string name)
+        {
+            Name = name;
+        }
+
+        public RequestMappingAttribute(string name, string url)
+        {
+            Name = name;
+            Url = url;
+        }
+
+    }
+}
---
+++ src/Moonlit.Mvc/RouteCollectionExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Compilation;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Moonlit.Mvc
+{
+    public class RequestRoutes
+    {
+        private List<RouteItem> _routes = new List<RouteItem>();
+        public void MapRequestMappings(RouteCollection route)
+        {
+            var referencedAssemblies = BuildManager.GetReferencedAssemblies();
+            MapRequestMappings(route, referencedAssemblies.Cast<Assembly>());
+        }
+
+        public static void MapRequestMappings(RouteCollection route, IEnumerable<Assembly> assemblies)
+        {
+            foreach (Assembly referencedAssembly in assemblies)
+            {
+                foreach (var exportedType in referencedAssembly.GetExportedTypes())
+                {
+                    var methodInfos = exportedType.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
+                    foreach (var methodInfo in methodInfos)
+                    {
+                        var requestMappingAttr = methodInfo.GetCustomAttribute<RequestMappingAttribute>(false);
+                        if (requestMappingAttr != null)
+                        {
+                            route.MapRoute(requestMappingAttr.Name,
+                                requestMappingAttr.Url ?? "",
+                                defaults:
+                                    new
+                                    {
+                                        controller = exportedType.Name.Replace("Controller", ""),
+                                        action = methodInfo.Name
+                                    }
+                                );
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public class RouteItem
+    {
+    }
+}
