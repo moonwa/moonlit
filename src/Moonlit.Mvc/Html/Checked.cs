c5fdfa7f34233f41b2bc5ed34cb70bfb2a6a1318
moon.wa <moon.ww.z@gmail.com>
2015-03-15 21:03:27 +0800

    add nuspec

---
+++ src/Moonlit.Mvc/Html/Checked.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace Moonlit.Mvc.Html
+{
+    public static class CheckedExtensions
+    {
+        public static string Checked(this HtmlHelper html, bool ischecked)
+        {
+            if (ischecked)
+            {
+                return "checked=\"checked\"";
+            }
+            return string.Empty;
+        }
+    }
+}
