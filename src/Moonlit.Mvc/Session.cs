600343d8c59954ecaf42e35def09f1ef23ff2582
wall-e zhan <moon.ww.z@gmail.com>
2015-06-16 19:48:16 +0800

    g

---
+++ src/Moonlit.Mvc/Session.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Moonlit.Mvc
+{
+    public class Session
+    {
+        public Session()
+        {
+            CreationTime = DateTime.Now;
+            ExpiredTime = CreationTime;
+        }
+
+        public string UserName { get; set; }
+        public string AppId { get; set; }
+        public DateTime CreationTime { get; set; }
+        public DateTime ExpiredTime { get; set; }
+        public string[] Privileges { get; set; }
+
+        public string SessionId { get; set; }
+    }
+}
