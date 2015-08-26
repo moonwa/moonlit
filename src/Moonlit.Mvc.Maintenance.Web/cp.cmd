53a5db62c04ea39c8aec87c5cb6e8c5de6efaa56
wall-e zhan <moon.ww.z@gmail.com>
2015-08-26 22:04:50 +0800

    g

--- src/Moonlit.Mvc.Maintenance.Web/cp.cmd
+++ src/Moonlit.Mvc.Maintenance.Web/cp.cmd
@@ -1,8 +1,8 @@
 copy C:\workspace\moonlit\src\Moonlit.Mvc.Maintenance.Web\bin\Release\Moonlit.dll C:\workspace\ecard\ecard-main-server\packages\moonlit.1.1.6.22\lib\net45\Moonlit.dll
 copy C:\workspace\moonlit\src\Moonlit.Mvc.Maintenance.Web\bin\Release\Moonlit.pdb C:\workspace\ecard\ecard-main-server\packages\moonlit.1.1.6.22\lib\net45\Moonlit.pdb
 
-copy C:\workspace\moonlit\src\Moonlit.Mvc.Maintenance.Web\bin\Release\Moonlit.Mvc.dll C:\workspace\ecard\ecard-main-server\packages\Moonlit.Mvc.1.0.4.6\lib\net451\Moonlit.Mvc.dll
-copy C:\workspace\moonlit\src\Moonlit.Mvc.Maintenance.Web\bin\Release\Moonlit.Mvc.pdb C:\workspace\ecard\ecard-main-server\packages\Moonlit.Mvc.1.0.4.6\lib\net451\Moonlit.Mvc.pdb
+copy C:\workspace\moonlit\src\Moonlit.Mvc.Maintenance.Web\bin\Release\Moonlit.Mvc.dll C:\workspace\ecard\ecard-main-server\packages\Moonlit.Mvc.1.0.4.7\lib\net451\Moonlit.Mvc.dll
+copy C:\workspace\moonlit\src\Moonlit.Mvc.Maintenance.Web\bin\Release\Moonlit.Mvc.pdb C:\workspace\ecard\ecard-main-server\packages\Moonlit.Mvc.1.0.4.7\lib\net451\Moonlit.Mvc.pdb
 
 copy C:\workspace\moonlit\src\Moonlit.Mvc.Maintenance.Web\bin\Release\Moonlit.Mvc.Maintenance.dll C:\workspace\ecard\ecard-main-server\packages\Moonlit.Mvc.Maintenance.1.0.4.11\lib\net451\Moonlit.Mvc.Maintenance.dll
 copy C:\workspace\moonlit\src\Moonlit.Mvc.Maintenance.Web\bin\Release\Moonlit.Mvc.Maintenance.pdb C:\workspace\ecard\ecard-main-server\packages\Moonlit.Mvc.Maintenance.1.0.4.11\lib\net451\Moonlit.Mvc.Maintenance.pdb
--- src/Moonlit.Mvc.Maintenance/Properties/AssemblyInfo.cs
+++ src/Moonlit.Mvc.Maintenance/Properties/AssemblyInfo.cs
@@ -38,8 +38,8 @@ using Moonlit.Mvc.Maintenance.Properties;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.4.11")]
-[assembly: AssemblyFileVersion("1.0.4.11")]
+[assembly: AssemblyVersion("1.0.4.12")]
+[assembly: AssemblyFileVersion("1.0.4.12")]
 
 
 [assembly: Privilege(Text = "PrivilegeAdminUser", Name = MaintPrivileges.PrivilegeAdminUser, ResourceType = typeof(MaintCultureTextResources))]
