1cda450013bd06b9aa61e78a903bdc36c6fa46cb
zhan zhang <moon.ww@hotmail.com>
2016-04-23 17:10:43 +0800

    g

---
+++ src/Moonlit.Mvc/ToEntityContext.cs
@@ -0,0 +1,9 @@
+using System.Web.Mvc;
+
+namespace Moonlit.Mvc
+{
+    public class ToEntityContext
+    {
+        public ControllerContext ControllerContext { get; set; }
+    }
+}
---
+++ src/Moonlit.Mvc/ToEntityExtensions.cs
@@ -0,0 +1,59 @@
+using System.Web.Mvc;
+
+namespace Moonlit.Mvc
+{
+    public static class ToEntityExtensions
+    {
+        public static void FromEntity<TModel, TEntity>(this TModel model, TEntity entity, FromEntityContext context)
+        {
+            var modelAccessor = EntityAccessor.GetAccessor(typeof(TModel));
+            var entityAccessor = EntityAccessor.GetAccessor(typeof(TEntity));
+
+            var properties = ModelMetadataProviders.Current.GetMetadataForProperties(model, typeof(TModel));
+            foreach (var propertyMatadata in properties)
+            {
+                var mappingTo = propertyMatadata.GetMapping();
+                if (mappingTo != null)
+                {
+                    if (!mappingTo.OnlyNotPostback || !context.IsPostback || propertyMatadata.IsReadOnly)
+                    {
+                        if (entityAccessor.HasPropertyGetter(mappingTo.To) && entityAccessor.HasPropertySetter(propertyMatadata.PropertyName))
+                        {
+                            var value = entityAccessor.GetProperty(entity, mappingTo.To ?? propertyMatadata.PropertyName);
+                            modelAccessor.SetProperty(model, propertyMatadata.PropertyName, value);
+                        }
+                    }
+                }
+            }
+            IFromEntity<TEntity> to = model as IFromEntity<TEntity>;
+            if (to != null)
+            {
+                to.OnFromEntity(entity, context);
+            }
+        }
+        public static void ToEntity<TModel, TEntity>(this TModel model, TEntity entity, ToEntityContext context)
+        {
+            var modelAccessor = EntityAccessor.GetAccessor(typeof(TModel));
+            var entityAccessor = EntityAccessor.GetAccessor(typeof(TEntity));
+
+            var properties = ModelMetadataProviders.Current.GetMetadataForProperties(model, typeof(TModel));
+            foreach (var propertyMatadata in properties)
+            {
+                if (propertyMatadata.IsReadOnly)
+                {
+                    continue;
+                }
+                var mappingTo = propertyMatadata.GetMapping();
+                if (mappingTo != null)
+                {
+                    entityAccessor.SetProperty(entity, mappingTo.To ?? propertyMatadata.PropertyName, propertyMatadata.Model);
+                }
+            }
+            IToEntity<TEntity> to = model as IToEntity<TEntity>;
+            if (to != null)
+            {
+                to.OnToEntity(entity, context);
+            }
+        }
+    }
+}
